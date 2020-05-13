9216818ca1244adbc342bf625ae406e3b836adcd
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-13 18:54:08 -0400

    Latest Update - Added travis file - Added dummy test file - Added Dockerfile to client folder - Updated mongo connection data - Created nginx server for client folder - Created Dockerfile for client folder - Updated docker compose file - Updated main nginx configuration for new service (client static server)

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+const { mongoURI } = require("../config/config");
 
 const connectToDb = async () => {
-  const connection = await mongoose.connect(mongoDBURI, {
+  const connection = await mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
